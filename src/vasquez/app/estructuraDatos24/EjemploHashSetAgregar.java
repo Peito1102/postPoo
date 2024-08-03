3d19441aa504a2ca066f27e25b2b74a82a268008
Peito1102 <renzitiwin@gmail.com>
2024-08-02 22:50:39 -0500

    parte 1 y 2 de estructuraDatos(24)

---
+++ src/vasquez/app/estructuraDatos24/EjemploHashSetAgregar.java
@@ -0,0 +1,26 @@
+package vasquez.app.estructuraDatos24;
+
+import java.util.*;
+
+public class EjemploHashSetAgregar {
+    public static void main(String[] args) {
+        Set<String> hs = new HashSet<>();
+        hs.add("uno");
+        hs.add("dos");
+        hs.add("tres");
+        hs.add("cuatro");
+        hs.add("cinco");
+        hs.add("seis");
+        hs.add("siete");
+        System.out.println(hs);
+        boolean a = hs.add("uno");
+        System.out.println(a);
+
+        List<String> lista = new ArrayList<>(hs);
+        Collections.sort(lista);
+        System.out.println(lista);
+        Collections.reverse(lista);
+        System.out.println(lista);
+
+    }
+}

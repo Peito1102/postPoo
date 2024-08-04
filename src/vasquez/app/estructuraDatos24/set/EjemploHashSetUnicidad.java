95e17de3c44a21d7d6bffe57efdaceaa72c8fa99
Peito1102 <i202210758@cibertec.edu.pe>
2024-08-04 02:05:47 -0500

    parte 7 de estructuraDatos(24)

--- src/vasquez/app/estructuraDatos24/set/EjemploHashSetUnicidad.java
+++ src/vasquez/app/estructuraDatos24/set/EjemploHashSetUnicidad.java
@@ -2,12 +2,13 @@ package vasquez.app.estructuraDatos24.set;
 
 import vasquez.app.estructuraDatos24.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
-public class EjemploHashSetUnicidad {
+public class EjemploHashSetUnicidad { //Obs: Set puede usar todos las iteraciones como el List menos el for clasico.
     public static void main(String[] args) {
         Set<Alumno> sa = new HashSet<>();
+        //List<Alumno> sa = new ArrayList<>();
         sa.add(new Alumno("JD",10));
         sa.add(new Alumno("Piero",8));
         sa.add(new Alumno("Renzo",2));
@@ -19,6 +20,33 @@ public class EjemploHashSetUnicidad {
         sa.add(new Alumno("Robotin",7));
 
         System.out.println(sa);
+        /*
+        System.out.println("For clasico");
+        for (int i = 0;i < sa.size();i++) {
+            Alumno a = sa.get(i);
+            System.out.println(a.getNombre());
+        }*/
+
+        System.out.println("Iterando con forEach");
+        for (Alumno a : sa) {
+            System.out.println(a);
+        }
+
+        System.out.println("Iterando con while y iterator");
+        Iterator<Alumno> it = sa.iterator();
+        while (it.hasNext()) {
+            Alumno a = it .next();
+            System.out.println(a);
+        }
+
+        do {
+            Alumno a = it .next();
+            System.out.println(a);
+        } while (it.hasNext());
+
+        System.out.println("Iterando usando Stream forEach");
+        sa.forEach(System.out::println);
+
     }
 
 }
